3bf9b8ccbfb61a6a168686d25daf944927ffff78
manoj7417 <manoj@glassfrog.design>
2025-06-06 12:46:45 +0530

    Changes in clarity ag

--- src/app/page.js
+++ src/app/page.js
@@ -33,81 +33,81 @@ export default function Home() {
       question: "Will I get assistance in choosing my specialization?",
       answer: "Yes, we at Gyanarthi  College believe in supporting our students in every aspect of their academic learning. Starting from the admission, our faculty is always there to assist students if they get stuck at some point."
     },
-    
+
   ];
-  
+
   return (
     <>
       <head>
         <script
-        id="clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "r8gqo9dp1g");
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r8gqo9dp1g");
           `,
-        }}
+          }}
         ></script>
         <script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-956907995"
-        strategy="afterInteractive"
-      />
-      <script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+          src="https://www.googletagmanager.com/gtag/js?id=AW-956907995"
+          strategy="afterInteractive"
+        />
+        <script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-956907995');
           `,
-        }}
+          }}
         ></script>
         <script id="google-ads-conversion" strategy="afterInteractive">
-  {`
+          {`
     gtag('event', 'conversion', {'send_to': 'AW-956907995/SkFMCM_ag-4CENuDpcgD'});
   `}
-</script>
+        </script>
       </head>
-     <Popup/>
+      <Popup />
       <Header />
       <Main />
-    
 
-     
-       <div className="py-16 bg-white">
-      <div className="container mx-auto px-6 max-w-7xl text-center">
-        <h2 className="text-3xl font-extrabold text-gray-800 mb-8">Frequently Asked Questions</h2>
 
-        {/* FAQ List */}
-        <div className="space-y-6">
-          {faqData.map((item, index) => (
-            <div key={index} className="border-b border-gray-300">
-              <div
-                onClick={() => toggleOpen(index)}
-                className="cursor-pointer flex justify-between items-center py-4 text-lg font-semibold text-gray-800 "
-              >
-                <span>{item.question}</span>
-                <span className={`transform transition-transform duration-300 ${open === index ? "rotate-45" : ""}`}>
-                  {/* Adding the "+" icon here */}
-                  <i className={`fas ${open === index ? "fa-minus" : "fa-plus"}`}></i>
-                </span>
+
+      <div className="py-16 bg-white">
+        <div className="container mx-auto px-6 max-w-7xl text-center">
+          <h2 className="text-3xl font-extrabold text-gray-800 mb-8">Frequently Asked Questions</h2>
+
+          {/* FAQ List */}
+          <div className="space-y-6">
+            {faqData.map((item, index) => (
+              <div key={index} className="border-b border-gray-300">
+                <div
+                  onClick={() => toggleOpen(index)}
+                  className="cursor-pointer flex justify-between items-center py-4 text-lg font-semibold text-gray-800 "
+                >
+                  <span>{item.question}</span>
+                  <span className={`transform transition-transform duration-300 ${open === index ? "rotate-45" : ""}`}>
+                    {/* Adding the "+" icon here */}
+                    <i className={`fas ${open === index ? "fa-minus" : "fa-plus"}`}></i>
+                  </span>
+                </div>
+
+                {open === index && (
+                  <div className="text-gray-600 text-left text-lg pb-4">{item.answer}</div>
+                )}
               </div>
-
-              {open === index && (
-                <div className="text-gray-600 text-left text-lg pb-4">{item.answer}</div>
-              )}
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-      <Footer />  
+      <Footer />
 
 
     </>
